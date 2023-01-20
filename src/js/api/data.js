9e31df01d5000bada3c9187622dad130876f2751
DeferredMonk <marcoangeli151@gmail.com>
2023-01-20 17:17:45 +0200

    feat: added styled inputfield and added database check for user + post new user feature

--- src/js/api/data.js
+++ src/js/api/data.js
@@ -3,9 +3,28 @@
  * @returns {Array<Object>} Array of objects
  */
 const fetchData = async () => {
-    let response = await fetch(`${process.env.REACT_APP_API_URL}/translations`)
-    const result = await response.json()
-    return result
-
-}
-export default fetchData
+  let response = await fetch(`${process.env.REACT_APP_API_URL}/translations`);
+  const result = await response.json();
+  return result;
+};
+const postData = async (toPost) => {
+  fetch(`${process.env.REACT_APP_API_URL}/translations`, {
+    method: "POST",
+    headers: {
+      "X-API-Key": process.env.REACT_APP_API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(toPost),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Could not create new user");
+      }
+      return response.json();
+    })
+    .then((newUser) => {
+      console.log(newUser);
+    })
+    .catch((error) => console.log(error));
+};
+export { fetchData, postData };
--- src/js/components/inputForm.js
+++ src/js/components/inputForm.js
@@ -1,5 +1,60 @@
-const InputForm = () => {
-    return <div className="inputForm"></div>
-}
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { postData } from "../api/data";
+import "../../sass/inputForm.sass";
 
-export default InputForm
+const InputForm = ({ setLoggedUser, data, loggedUser }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (inputData) => {
+    const userNameInUse = data.filter(
+      (user) => user.username.toLowerCase() === inputData.username.toLowerCase()
+    );
+    if (userNameInUse.length === 0) {
+      //postData(inputData);
+      console.log("post");
+    }
+    setLoggedUser(inputData.username);
+  };
+  console.log(loggedUser);
+  //Error handlers
+  //if (errors.message !== "") console.log("test");
+
+  return (
+    <div className="inputForm">
+      {loggedUser ? (
+        <form className="logged" onSubmit={handleSubmit(onSubmit)}>
+          <div className="inputContainer">
+            <input
+              autoComplete="off"
+              className="inputElem"
+              {...register("translation", {
+                required: "What would you like to translate?",
+              })}
+            />
+            <button type="submit" className="translateButton">Translate</button>
+          </div>
+        </form>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="inputContainer">
+            <input
+              autoComplete="off"
+              className="inputElem"
+              {...register("username", {
+                required: "Please input your username!",
+              })}
+            />
+            <button type="submit">Sign in</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default InputForm;
