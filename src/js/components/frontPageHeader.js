4cd4c1375e4027e07398cf7dfb6d4d9f8ecd7e0c
DeferredMonk <marcoangeli151@gmail.com>
2023-01-24 10:28:04 +0200

    feat: frontpageheader ready for merge

--- src/js/components/frontPageHeader.js
+++ src/js/components/frontPageHeader.js
@@ -4,10 +4,13 @@ import logo from "../../Assets/Logo-Hello.png";
 const FrontPageHeader = ({ loggedUser }) => {
   return (
     <div className={!loggedUser ? "frontPageHeader" : "frontPageHeader hidden"}>
-      <img src={logo} alt="hello logo" className={"imgLogo"} />
-      <h1 className={"title"}>
-        Lost <span className="smallIn">in</span> translation
-      </h1>
+      <img src={logo} alt="hello logo" className="imgLogo" />
+      <div className="titleAndDescContainer">
+        <h1 className="title">
+          Lost <span className="smallIn">in</span> translation
+        </h1>
+        <p>Sign in and start translating!</p>
+      </div>
     </div>
   );
 };
