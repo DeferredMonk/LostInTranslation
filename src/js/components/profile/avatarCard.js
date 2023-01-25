6953f5cfa588674fc7d755d70ce4ffe907944d3e
Kristian Wink <kristian.wink@fi.experis.com>
2023-01-25 16:27:34 +0200

    avatarCard component changes

--- src/js/components/profile/avatarCard.js
+++ src/js/components/profile/avatarCard.js
@@ -1,12 +1,34 @@
-const AvatarCard = ({loggedUser}) => {
-    return (
-      <div className="avatarCard">
-        <p>{loggedUser && loggedUser.username}</p>
-        <div>
-         
-        </div>
-      </div>
-    )
-}
+import "../../../sass/avatar.sass";
+import { patchData } from "../../api/data.js";
 
-export default AvatarCard
+const AvatarCard = ({ loggedUser }) => {
+  const clearHistory = () => {
+    patchData(loggedUser.id, []);
+  };
+
+  return (
+    <div className="avatarCard">
+      <div className="circleBase circle">
+        <span className="firstLetter">
+          {loggedUser && loggedUser.username[0]}
+        </span>
+      </div>
+      <div className="userInfoContainer">
+        <span className="infoText">Username:</span>
+        <span className="userProfileName">
+          {loggedUser && loggedUser.username}
+        </span>
+        <span className="infoText">
+          Translations count:{" "}
+          {loggedUser && loggedUser.translations.length}
+        </span>
+      </div>
+      <div className="buttonContainer" onClick={clearHistory}>
+        <button>Clear Translations</button>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default AvatarCard;
