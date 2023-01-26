673f031521b4d5586b8b602b4039159f68425fc5
DeferredMonk <marcoangeli151@gmail.com>
2023-01-26 15:32:11 +0200

    fix: fixed divs and optimized code

--- src/js/components/translations/translatedSign.js
+++ src/js/components/translations/translatedSign.js
@@ -1,29 +1,38 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { clearTranslation } from "../../reducers/translationSlice"
+import { useDispatch, useSelector } from "react-redux";
+import { clearTranslation } from "../../reducers/translationSlice";
 import "../../../sass/translatedSign.sass";
 
-
 const TranslatedSign = () => {
-    const { translation } = useSelector(state => state.translation)
-    const translationCharts = translation.split('')
-    const dispatch = useDispatch()
+  const { translation } = useSelector((state) => state.translation);
+  const translationCharts = translation.split("");
+  const dispatch = useDispatch();
 
-    const translate = (input) => {
-        if (input === ' ') return <div></div>
-        return <img className="sign" src={require(`../../../Assets/individial_signs/${input.toLowerCase()}.png`)} />
-    }
+  const translate = (input) => {
+    if (input === " ") return <div className="sign"></div>;
+    return (
+      <img
+        className="sign"
+        alt={input.toLowerCase() + " sign"}
+        src={require(`../../../Assets/individial_signs/${input.toLowerCase()}.png`)}
+      />
+    );
+  };
 
-    return <div className="translatedSign">
-        <div className="translationContainer"> 
-            {translation 
-                ? <>{translationCharts.map(char => translate(char))}
-                <button className="clearButton" onClick={() => dispatch(clearTranslation())}>Clear</button>
-                </>
-                : <></>
-            }
-            
-        </div>
+  return (
+    <div className="translatedSign">
+      <div className="translationContainer">
+        {translation && <>{translationCharts.map((char) => translate(char))}</>}
+      </div>
+      <button
+        className="clearButton"
+        onClick={() => {
+          dispatch(clearTranslation());
+        }}
+      >
+        Clear
+      </button>
     </div>
-}
+  );
+};
 
-export default TranslatedSign
+export default TranslatedSign;
