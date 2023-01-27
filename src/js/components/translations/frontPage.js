72f6b7d5bc70265e94e5b2d37f34fb00873fbd41
DeferredMonk <marcoangeli151@gmail.com>
2023-01-27 16:07:29 +0200

    Docs:Updated documentation on all translation folder components

--- src/js/components/translations/frontPage.js
+++ src/js/components/translations/frontPage.js
@@ -2,6 +2,11 @@ import { useSelector } from "react-redux";
 import InputForm from "./inputForm.js";
 import TranslatedSign from "./translatedSign.js";
 
+/**
+ * Renders inputForm element &
+ * If user logged renders translatedSign component
+ * @returns {JSX-Element}
+ */
 const FrontPage = () => {
   const {id} = useSelector((state) => state.user)
 
